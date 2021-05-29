85f0c46f1ded722dc02e9e8b023fa422d28b16b1
nadee <nadeem.jazmawe@gmail.com>
2021-05-30 00:45:03 +0300

    creact react app

---
+++ src/calculator.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class calculator extends Component {
+    render() {
+        return (
+            <div>
+                my calculator
+            </div>
+        )
+    }
+}
+
+export default calculator
